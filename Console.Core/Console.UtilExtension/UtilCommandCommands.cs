4700c455e61328fd126c56015752d97286fe5366
Byt3ChkR <ti.akermann@gmail.com>
2020-08-20 19:40:17 +0200

    Added Commands to Remove Commands Utils Functions are now also exposed as command. Utils Script can now unload itself.

--- Console.Core/Console.UtilExtension/UtilCommandCommands.cs
+++ Console.Core/Console.UtilExtension/UtilCommandCommands.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Linq;
 using Console.Core.CommandSystem;
+using Console.Core.CommandSystem.Commands;
 
 namespace Console.UtilExtension
 {
@@ -12,11 +14,29 @@ namespace Console.UtilExtension
         /// Adds all commands of a specified type
         /// </summary>
         /// <param name="qualifiedName">Assembly Qualified Type Name</param>
-        [Command("add-commands", "Adds all static Console Commands of the specified Type")]
+        [Command("add-command", "Adds all static Console Commands of the specified Type")]
         private static void AddCommands(string qualifiedName)
         {
             Type t = Type.GetType(qualifiedName);
             CommandAttributeUtils.AddCommands(t);
+        }
+
+        [Command("remove-command", "Removes a command with this name and counts.")]
+        private static void RemoveCommand(string name, int parameterCount, int flagCount)
+        {
+            AbstractCommand cd = CommandManager.GetCommand(name, parameterCount, flagCount);
+            CommandManager.RemoveCommand(cd);
+        }
+
+        [Command("remove-command", "Removes a command with this name and parameter count.")]
+        private static void RemoveCommand(string name, int parameterCount) => RemoveCommand(name, parameterCount, 0);
+        [Command("remove-command", "Removes a command with this name and no parameters.")]
+        private static void RemoveCommand(string name) => RemoveCommand(name, 0);
+
+        [Command("remove-commands", "Removes all commands with this name.")]
+        private static void RemoveCommands(string name)
+        {
+            CommandManager.GetCommands(name).ToList().ForEach(CommandManager.RemoveCommand);
         }
     }
 }
